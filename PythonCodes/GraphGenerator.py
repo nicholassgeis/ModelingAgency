824e3f427938bfa0ff87933453dc79146d186973
nicholassgeis <nsg@umail.ucsb.edu>
2018-02-09 22:22:48 -0800

    py code returns chart

--- PythonCodes/GraphGenerator.py
+++ PythonCodes/GraphGenerator.py
@@ -8,26 +8,41 @@ wb = load_workbook('ProblemCData.xlsx')
 seseds = wb[wb.sheetnames[0]]
 msncodes = wb[wb.sheetnames[1]]
 
-testgraph = wb.create_sheet("Graph")
+graphs = wb.create_sheet("Graphs")
 
 chart = ScatterChart()
 chart.style = 13
 
-for i in range(2, 50):
+for i in range(2, 52):
 
-    code = seseds.cell(row=i, column=1)
+    c = seseds.cell(row=i, column=1)
+    d = seseds.cell(row=i, column=2)
 
-    for j in range(2, 606):
+    code = c.value
+    state = d.value
 
-        if code == msncodes.cell(row=j, column=1):
+    x = Reference(seseds, min_col=3, min_row = 2, max_row=51)
+    y = Reference(seseds, min_col=4, min_row = 2, max_row=51)
+    s = Series(y, xvalues=x)
+    chart.append(s)
 
-            chart.title = "msncodes.cell(row=i, column=2)"
+    for j in range(2, 607):
+
+        cd = msncodes.cell(row=j, column=1)
+        title = msncodes.cell(row=j, column=2)
+        y_axis = msncodes.cell(row=j, column = 3)
+
+        if code == cd.value:
+
+            newtitle = (title.value + " " + state)
+
+            chart.title = newtitle
             chart.x_axis.title = 'Years'
-            chart.y_axis.title = 'msncodes.cell(row=i, column=3)'
+            chart.y_axis.title = y_axis.value
 
             ## This will just check every cell. Not sure how to be smart
 
-    chart.add_data(seseds.cell(row=i, column=2), seseds.cell(row=i, column=3))
+    
 
 testgraph.add_chart(chart, "A1")
 wb.save("Test.xlsx")
