e50e46bf8071c242978b752a170483a01462d241
nicholassgeis <nsg@umail.ucsb.edu>
2018-02-10 00:06:01 -0800

    Creates perfect graphs

--- PythonCodes/GraphGenerator.py
+++ PythonCodes/GraphGenerator.py
@@ -1,5 +1,6 @@
 from openpyxl import (load_workbook, Workbook)
 from openpyxl.chart import( ScatterChart, Reference, Series)
+import math
 
 wb = load_workbook('ProblemCData.xlsx')
 
@@ -10,39 +11,53 @@ msncodes = wb[wb.sheetnames[1]]
 
 graphs = wb.create_sheet("Graphs")
 
-chart = ScatterChart()
-chart.style = 13
+numberof4s = 0
 
-for i in range(2, 52):
+for k in range(0, 5):
 
-    c = seseds.cell(row=i, column=1)
-    d = seseds.cell(row=i, column=2)
+    if (k%4==0):
+
+        numberof4s+=1
+
+    chart = ScatterChart()
+    chart.style = 13
+
+    ## Defines msn code and state for kth 50 chunk
+
+    c = seseds.cell(row=(50*k+2), column=1)
+    d = seseds.cell(row=(50*k+2), column=2)
 
     code = c.value
     state = d.value
 
-    x = Reference(seseds, min_col=3, min_row = 2, max_row=51)
-    y = Reference(seseds, min_col=4, min_row = 2, max_row=51)
-    s = Series(y, xvalues=x)
+    ## Create data coordinates for chart and appends data to chart
+
+    year = Reference(seseds, min_col=3, min_row = (50*k+2), max_row=(50*(k+1)+1))
+    data = Reference(seseds, min_col=4, min_row = (50*k+2), max_row=(50*(k+1)+1))
+    s = Series(data, xvalues=year)
     chart.append(s)
 
-    for j in range(2, 607):
+    ## Collects corresponding despriction and units from msncodes
 
-        cd = msncodes.cell(row=j, column=1)
-        title = msncodes.cell(row=j, column=2)
-        y_axis = msncodes.cell(row=j, column = 3)
+    title = msncodes.cell(row=(numberof4s+1), column=2)
+    y_axis = msncodes.cell(row=(numberof4s+1), column=3)
 
-        if code == cd.value:
+    ## Defines new title that will Description followed by state
 
-            newtitle = (title.value + " " + state)
+    newtitle = (title.value + " " + state)
 
-            chart.title = newtitle
-            chart.x_axis.title = 'Years'
-            chart.y_axis.title = y_axis.value
+    ## Gives appropriate names to title, x-axis and y-axis for each chart
 
-            ## This will just check every cell. Not sure how to be smart
+    chart.title = newtitle
+    chart.x_axis.title = 'Years'
+    chart.y_axis.title = y_axis.value
 
-    
+    ## Spaces charts out appropriately to fit two per page
 
-testgraph.add_chart(chart, "A1")
+    numberforA = 17*k+1
+
+    chartposition = "A" + str(numberforA)
+
+    graphs.add_chart(chart, chartposition)
+
 wb.save("Test.xlsx")
